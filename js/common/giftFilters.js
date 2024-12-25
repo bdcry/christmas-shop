0b77109e1a5cd185cdd60f7af64cfe3dc4dfe81e
Tim <tim@MacBook-Pro-2.local>
2024-12-26 00:11:12 +0300

    Add gift card filters functionallity

---
+++ js/common/giftFilters.js
@@ -0,0 +1,28 @@
+const handleTabClick = (tabs, tab, cards) => {
+  tabs.forEach((tab) => tab.classList.remove('active_filter_btn'));
+  tab.classList.add('active_filter_btn');
+
+  const selectedCategory = tab.dataset.category;
+
+  cards.forEach((card) => {
+    if (selectedCategory === 'All' || selectedCategory === card.dataset.category) {
+      card.classList.add('filter_category_active');
+      card.classList.remove('filter_category_disable');
+    } else {
+      card.classList.add('filter_category_disable');
+    }
+  })
+};
+
+
+const initGiftFilters = () => {
+  const tabs = document.querySelectorAll('.filter_btn');
+  const cards = document.querySelectorAll('.gift_card');
+
+
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => handleTabClick(tabs, tab, cards));
+  });
+};
+
+export default initGiftFilters;
--- js/pages/giftsPage.js
+++ js/pages/giftsPage.js
@@ -1,5 +1,7 @@
+import initGiftFilters from "../common/giftFilters.js";
 import { goTop } from "../common/goTop.js"
 import { initGiftModal } from "../common/popupCard.js";
 
 goTop();
 initGiftModal();
+initGiftFilters();
